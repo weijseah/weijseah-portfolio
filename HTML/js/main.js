5011176e278e58385ab20cf416805a5e25ced92e
weijseah <weijseah@gmail.com>
2018-01-07 17:25:00 -0500

    Added pictures and updated navigation bar

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -1,32 +1,4 @@
 jQuery(document).ready(function($){
-	//if you change this breakpoint in the style.css file (or _layout.scss if you use SASS), don't forget to update this value as well
-	var MQL = 1170;
-
-	//primary navigation slide-in effect
-	if($(window).width() > MQL) {
-		var headerHeight = $('.cd-header').height();
-		$(window).on('scroll',
-		{
-	        previousTop: 0
-	    }, 
-	    function () {
-		    var currentTop = $(window).scrollTop();
-		    //check if user is scrolling up
-		    if (currentTop < this.previousTop ) {
-		    	//if scrolling up...
-		    	if (currentTop > 0 && $('.cd-header').hasClass('is-fixed')) {
-		    		$('.cd-header').addClass('is-visible');
-		    	} else {
-		    		$('.cd-header').removeClass('is-visible is-fixed');
-		    	}
-		    } else {
-		    	//if scrolling down...
-		    	$('.cd-header').removeClass('is-visible');
-		    	if( currentTop > headerHeight && !$('.cd-header').hasClass('is-fixed')) $('.cd-header').addClass('is-fixed');
-		    }
-		    this.previousTop = currentTop;
-		});
-	}
 	
 	// Smooth transition for navbar when scrolling down/back to header
 	$(function () {
